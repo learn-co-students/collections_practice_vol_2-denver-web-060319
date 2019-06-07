624912b2a0fdf55599b71993f577d715365fd153
“Patrick <pctennislessons@gmail.com>
2019-06-07 11:05:47 -0600

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,7 +49,42 @@ def count_elements array
 end
 
 def merge_data keys, data 
-    binding.pry
-    keys 
-    data
+    data.map do |hash_in_data|
+        hash_in_data.each do |string, attributes|
+            keys.each do |hashes_in_keys|
+                if string == hashes_in_keys[:first_name]
+                    hash_in_data[string][:motto] = hashes_in_keys[:motto]
+                    hash_in_data[string][:first_name] = hashes_in_keys[:first_name]
+                end
+            end
+        end.values
+    end.flatten
 end
+
+def find_cool cool 
+    cool.select do |hash|
+        hash[:temperature] == "cool"
+    end
+end 
+
+def organize_schools schools 
+    binding.pry
+    schools.map do |school, location| 
+        location
+    end.inject(:merge)
+
+end
+# {"Chicago"=>["dev boot camp chicago"], 
+# "NYC"=>["flatiron school bk", "flatiron school", "general assembly"], 
+# "SF"=>["dev boot camp", "Hack Reactor"]}
+
+# {"flatiron school bk"=>{:location=>"NYC"},
+#  "flatiron school"=>{:location=>"NYC"},
+#  "dev boot camp"=>{:location=>"SF"},
+#  "dev boot camp chicago"=>{:location=>"Chicago"},
+#  "general assembly"=>{:location=>"NYC"},
+#  "Hack Reactor"=>{:location=>"SF"}}
+
+# hash.each_with_object({}) do |(k, v), a|
+#     a[k] = v + 3
+#    end 
