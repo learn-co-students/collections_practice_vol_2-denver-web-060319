c8da25441965b88d904c20d62d9b85ccc0efdf9d
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-12-27 17:33:14 -0700

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
 # your code goes here
+
+def begins_with_r(words)
+    words.all? {|x| x[0] == "r".downcase }
+end
+
+def contain_a(words)
+    words.select { |x| x.include?("a") }
+end
+
+def first_wa(words)
+    words.find { |x| x[0..1] == "wa" }
+end
+
+
+def remove_non_strings(words)
+  words.delete_if { |x| x.class != "".class }
+end
+
+
+
+def count_elements(words)
+    hash = {}
+    words.uniq.map do |x|
+        hash = { :count => words.count(element),    }
+    end
+end
